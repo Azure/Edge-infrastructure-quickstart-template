30bd99d2cf36e62f58bb0e6e3014960474027630
Ke Qu <quke@microsoft.com>
2023-11-23 16:41:38 +0800

    update

--- modules/hci/deploy.tf
+++ modules/hci/deploy.tf
@@ -2,7 +2,7 @@ resource "azapi_update_resource" "deploymentsetting" {
   type       = "Microsoft.AzureStackHCI/clusters/deploymentSettings@2023-08-01-preview"
   name       = "default"
   parent_id  = azapi_resource.cluster1.id
-  depends_on = [azapi_resource.validatedeploymentsetting]
+  depends_on = [azapi_resource.validatedeploymentsetting, azapi_resource.validatedeploymentsetting_seperate]
   timeouts {
     create = "24h"
     update = "24h"
