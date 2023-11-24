1a085a3c3693655a464dbc2ace1a90145baad0a7
Ke Qu <quke@microsoft.com>
2023-11-24 12:09:57 +0800

    try

--- Dev/sample/backend.tf
+++ Dev/sample/backend.tf
@@ -1,8 +1,10 @@
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraformbackend"
     storage_account_name = "demobackend"
     container_name       = "tfbackend"
-    key                  = "terraform.tfstate"
+    key                  = "sample.tfstate"
   }
 }
+
