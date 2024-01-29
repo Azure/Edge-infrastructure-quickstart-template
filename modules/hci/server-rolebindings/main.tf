5801a7de3c92dd04594bdd2744905fbe5c77b0bb
Zidong Lu <zilu@microsoft.com>
2024-01-29 14:28:53 +0800

    change for_each key for better life

--- modules/hci/server-rolebindings/main.tf
+++ modules/hci/server-rolebindings/main.tf
@@ -4,15 +4,14 @@ data "azurerm_arc_machine" "server" {
 }
 
 locals {
-  RoleList = [
-    "Azure Connected Machine Resource Manager",
-    # "Azure Stack HCI Device Management Role", # This role is added by Arc installer
-    "Key Vault Secrets User",
-  ]
+  Roles = {
+    ACMRM = "Azure Connected Machine Resource Manager",
+    KVSU  = "Key Vault Secrets User",
+  }
 }
 
 resource "azurerm_role_assignment" "MachineRoleAssign" {
-  for_each             = toset(local.RoleList)
+  for_each             = local.Roles
   scope                = "/subscriptions/${var.subId}/resourceGroups/${var.resourceGroup.name}"
   role_definition_name = each.value
   principal_id         = data.azurerm_arc_machine.server.identity[0].principal_id
