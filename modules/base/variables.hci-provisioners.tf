41d0713b5f9cb19869be59250192371edbcff6f7
Zidong Lu <zilu@microsoft.com>
2024-01-30 15:05:56 +0800

    rearrange variables

--- modules/base/variables.hci-provisioners.tf
+++ modules/base/variables.hci-provisioners.tf
@@ -1,72 +1,77 @@
-# variable "location"               "ref/main/location"
-# variable "siteId"                 "ref/main/siteId"
-# variable "siteName"               "ref/main/siteName"
-# variable "subscriptionId"         "ref/main/subscriptionId"
-# variable "servers"                "ref/hci/servers"
-# variable "deploymentUserName"     "ref/hci/deploymentUserName"
-# variable "deploymentUserPassword" "ref/hci/deploymentUserPassword"
-# variable "localAdminUser"         "ref/hci/localAdminUser"
-# variable "localAdminPassword"     "ref/hci/localAdminPassword"
-# variable "domainFqdn"             "ref/hci/domainFqdn"
-# variable "adouPath"               "ref/hci/adouPath"
-# variable "tenant"                 "ref/hci/tenant"
-# variable "servicePrincipalId"     "ref/hci/servicePrincipalId"
-# variable "servicePrincipalSecret" "ref/hci/servicePrincipalSecret"
-
-variable "enableProvisioners" {
-  type        = bool
-  default     = true
-  description = "Whether to enable provisioners."
-}
-
-variable "domainServerIP" {
-  description = "The ip of the domain server."
-  type        = string
-}
-
-variable "destory_adou" {
-  description = "whether destroy previous adou"
-  default     = false
-  type        = bool
-}
-
-variable "domainAdminUser" {
-  sensitive   = true
-  type        = string
-  description = "The username for the domain administrator account."
-}
-
-variable "domainAdminPassword" {
-  sensitive   = true
-  type        = string
-  description = "The password for the domain administrator account."
-}
-
-variable "authenticationMethod" {
-  type        = string
-  description = "The authentication method for Enter-PSSession."
-  validation {
-    condition     = can(regex("^(Default|Basic|Negotiate|NegotiateWithImplicitCredential|Credssp|Digest|Kerberos)$", var.authenticationMethod))
-    error_message = "Value of authenticationMethod should be {Default | Basic | Negotiate | NegotiateWithImplicitCredential | Credssp | Digest | Kerberos}"
+# Potential global variables
+  variable "enableProvisioners" {
+    type        = bool
+    default     = true
+    description = "Whether to enable provisioners."
   }
-  default = "Default"
-}
+
+  variable "domainServerIP" {
+    description = "The ip of the domain server."
+    type        = string
+  }
+
+  variable "destory_adou" {
+    description = "whether destroy previous adou"
+    default     = false
+    type        = bool
+  }
+
+  variable "authenticationMethod" {
+    type        = string
+    description = "The authentication method for Enter-PSSession."
+    validation {
+      condition     = can(regex("^(Default|Basic|Negotiate|NegotiateWithImplicitCredential|Credssp|Digest|Kerberos)$", var.authenticationMethod))
+      error_message = "Value of authenticationMethod should be {Default | Basic | Negotiate | NegotiateWithImplicitCredential | Credssp | Digest | Kerberos}"
+    }
+    default = "Default"
+  }
+
+# Pass through variables
+  variable "domainAdminUser" {
+    sensitive   = true
+    type        = string
+    description = "The username for the domain administrator account."
+  }
+
+  variable "domainAdminPassword" {
+    sensitive   = true
+    type        = string
+    description = "The password for the domain administrator account."
+  }
 
 # Virtual host related variables
-variable "virtualHostIp" {
-  type        = string
-  description = "The virtual host IP address."
-  default     = ""
-}
+  # Site specific
+  variable "virtualHostIp" {
+    type        = string
+    description = "The virtual host IP address."
+    default     = ""
+  }
 
-variable "dcPort" {
-  type        = number
-  description = "Domain controller winrm port in virtual host"
-  default     = 5985
-}
+  # Potential global
+  variable "dcPort" {
+    type        = number
+    description = "Domain controller winrm port in virtual host"
+    default     = 5985
+  }
 
-variable "serverPorts" {
-  type        = map(number)
-  description = "Server winrm ports in virtual host"
-  default     = {}
-}
+  # Potential global
+  variable "serverPorts" {
+    type        = map(number)
+    description = "Server winrm ports in virtual host"
+    default     = {}
+  }
+# Reference variables
+  # variable "location"               "ref/main/location"
+  # variable "siteId"                 "ref/main/siteId"
+  # variable "siteName"               "ref/main/siteName"
+  # variable "subscriptionId"         "ref/main/subscriptionId"
+  # variable "servers"                "ref/hci/servers"
+  # variable "deploymentUserName"     "ref/hci/deploymentUserName"
+  # variable "deploymentUserPassword" "ref/hci/deploymentUserPassword"
+  # variable "localAdminUser"         "ref/hci/localAdminUser"
+  # variable "localAdminPassword"     "ref/hci/localAdminPassword"
+  # variable "domainFqdn"             "ref/hci/domainFqdn"
+  # variable "adouPath"               "ref/hci/adouPath"
+  # variable "tenant"                 "ref/hci/tenant"
+  # variable "servicePrincipalId"     "ref/hci/servicePrincipalId"
+  # variable "servicePrincipalSecret" "ref/hci/servicePrincipalSecret"
--- modules/base/variables.hci-vm.tf
+++ modules/base/variables.hci-vm.tf
@@ -1,5 +1,9 @@
-variable "enableVM" {
-  description = "Whether to enable VM."
-  type        = bool
-  default     = false
-}
+# Potential global variables
+  variable "enableVM" {
+    description = "Whether to enable VM."
+    type        = bool
+    default     = false
+  }
+
+# Site specific variables
+# Pass through variables
