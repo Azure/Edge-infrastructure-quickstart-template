d6e6cf11f0401ea644408474e1f607e968a968f5
Ke Qu <quke@microsoft.com>
2024-01-31 16:41:44 +0800

    add aks sample in main.tf

--- dev/sample/main.tf
+++ dev/sample/main.tf
@@ -50,15 +50,16 @@
 
 
 ## Region Hybrid AKS related parameters
-#   hybridAks-controlPlaneIp = "<hybridAks-controlPlaneIp>"
+#   enableAksArc                            = true
+#   aksArc-controlPlaneIp                   = "<aksArc-controlPlaneIp>"
 # 
-#   hybridAks-lnet-usingExistingLogicalNetwork = false
-#   // the following value only need to provide if you want to create a new logical network, if not, set hybridAks-lnet-usingExistingLogicalNetwork to true and specify the existing logical network name in logicalNetworkName
-#   hybridAks-lnet-startingAddress = "<hybridAks-lnet-startingAddress>"
-#   hybridAks-lnet-endingAddress = "<hybridAks-lnet-endingAddress>"
-#   hybridAks-lnet-addressPrefix = "<hybridAks-lnet-addressPrefix>"
-#   hybridaks-lnet-defaultGateway = "<hybridaks-lnet-defaultGateway>"
-#   hybridaks-lnet-dnsServers = ["<hybridaks-lnet-dnsServer>"]
+#   aksArc-lnet-usingExistingLogicalNetwork = false
+#   // the following value only need to provide if you want to create a new logical network, if not, set aksArc-lnet-usingExistingLogicalNetwork to true and specify the existing logical network name in logicalNetworkName
+#   aksArc-lnet-startingAddress             = "<aksArc-lnet-startingAddress>"
+#   aksArc-lnet-endingAddress               = "<aksArc-lnet-endingAddress>"
+#   aksArc-lnet-addressPrefix               = "<aksArc-lnet-addressPrefix>"
+#   aksArc-lnet-defaultGateway              = "<aksArc-lnet-defaultGateway>"
+#   aksArc-lnet-dnsServers                  = ["<aksArc-lnet-dnsServer>"]
 
 ## end region of hybrid aks related parameters
 # }
