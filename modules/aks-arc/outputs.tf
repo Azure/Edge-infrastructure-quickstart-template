1eebf56ed7635b73730f5a52a5ed18d8c9b51f2a
Zidong Lu <zilu@microsoft.com>
2024-03-01 14:13:36 +0800

    wait 10s before list admin kubeconfig

--- modules/aks-arc/outputs.tf
+++ modules/aks-arc/outputs.tf
@@ -8,14 +8,22 @@ output "aksCluster" {
   description = "Hybrid AKS Cluster instance"
 }
 
+// listAdminKubeconfig will raise 409 if run just after the cluster creation.
+resource "time_sleep" "wait_10_seconds" {
+  depends_on      = [azapi_resource.provisionedClusterInstance]
+  create_duration = "10s"
+}
+
 resource "azapi_resource_action" "kubeconfig" {
+  depends_on             = [time_sleep.wait_10_seconds]
   type                   = "Microsoft.HybridContainerService/provisionedClusterInstances@2024-01-01"
   resource_id            = azapi_resource.provisionedClusterInstance.id
   action                 = "listAdminKubeconfig"
   method                 = "POST"
   response_export_values = ["kubeconfigs"]
 }
-output "kubeConfig" {
+
+output "adminKubeConfig" {
   value     = resource.azapi_resource_action.kubeconfig.output
   sensitive = true
 }
