e3cb3f9399e55158ada310a4793707b7bd94af89
Zidong Lu <zilu@microsoft.com>
2024-03-27 13:22:26 +0800

    remove arb extension update

--- modules/aks-arc/extension.tf
+++ modules/aks-arc/extension.tf
@@ -1,18 +1,3 @@
-resource "azapi_update_resource" "k8sExtension" {
-  count     = var.isExported ? 0 : 1
-  type      = "Microsoft.KubernetesConfiguration/extensions@2023-05-01"
-  parent_id = var.arbId
-  name      = "hybridaksextension"
-  body = jsonencode({
-    properties = {
-      autoUpgradeMinorVersion = false
-      releaseTrain            = "stable"
-      version                 = "1.0.36"
-    }
-  })
-  timeouts {}
-}
-
 resource "terraform_data" "replacement" {
   count = var.isExported ? 0 : 1
   input = var.resourceGroup.name
@@ -25,7 +10,6 @@ locals {
 // this is a known issue for arc aks, it need to wait for the kubernate vhd ready to deploy aks
 resource "terraform_data" "waitAksVhdReady" {
   count      = var.isExported ? 0 : 1
-  depends_on = [azapi_update_resource.k8sExtension]
   provisioner "local-exec" {
     command     = "powershell.exe -ExecutionPolicy Bypass -NoProfile -File ${path.module}/readiness.ps1 -customLocationResourceId ${var.customLocationId} -kubernetesVersion ${var.kubernetesVersion} -osSku ${local.osSku}"
     interpreter = ["PowerShell", "-Command"]
