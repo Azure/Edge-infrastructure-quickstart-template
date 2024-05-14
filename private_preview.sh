502109d75ddff029d9f5bc4973c660d173dee29e
Zidong Lu <zilu@microsoft.com>
2024-05-14 20:40:09 +0800

    add amd support for private preview binaries (#66)

--- private_preview.sh
+++ private_preview.sh
@@ -1,2 +1,10 @@
-wget "https://aka.ms/az-edge-module-export-linux-amd64?$1" -O az-edge-module-export
-wget "https://aka.ms/az-edge-site-scale-linux-amd64?$1" -O az-edge-site-scale
+arch=$(uname -m)
+
+if [[ $arch == *"amd"* ]]; then
+    wget "https://aka.ms/az-edge-module-export-linux-arm64?$1" -O az-edge-module-export
+    wget "https://aka.ms/az-edge-site-scale-linux-arm64?$1" -O az-edge-site-scale
+else
+    wget "https://aka.ms/az-edge-module-export-linux-amd64?$1" -O az-edge-module-export
+    wget "https://aka.ms/az-edge-site-scale-linux-amd64?$1" -O az-edge-site-scale
+fi
+
