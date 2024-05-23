bacdf7f7fa1fc5fe316b8305feeb0b8215a8dbd9
Zidong Lu <zilu@microsoft.com>
2024-05-23 13:56:18 +0800

    fix binary downloading for macos (#75)

--- private_preview.sh
+++ private_preview.sh
@@ -1,10 +1,21 @@
 arch=$(uname -m)
 
-if [[ $arch == *"arm"* ]]; then
-    wget "https://aka.ms/az-edge-module-export-linux-arm64?$1" -O az-edge-module-export
-    wget "https://aka.ms/az-edge-site-scale-linux-arm64?$1" -O az-edge-site-scale
+if [[ $OSTYPE == *"linux"* ]]; then
+    if [[ $arch == *"arm"* ]]; then
+        wget "https://aka.ms/az-edge-module-export-linux-arm64?$1" -O az-edge-module-export
+        wget "https://aka.ms/az-edge-site-scale-linux-arm64?$1" -O az-edge-site-scale
+    else
+        wget "https://aka.ms/az-edge-module-export-linux-amd64?$1" -O az-edge-module-export
+        wget "https://aka.ms/az-edge-site-scale-linux-amd64?$1" -O az-edge-site-scale
+    fi
+elif [[ $OSTYPE == *"darwin"* ]]; then
+    if [[ $arch == *"arm"* ]]; then
+        wget "https://aka.ms/az-edge-module-export-darwin-arm64?$1" -O az-edge-module-export
+        wget "https://aka.ms/az-edge-site-scale-darwin-arm64?$1" -O az-edge-site-scale
+    else
+        echo "Unsupported architecture for macOS"
+    fi
 else
-    wget "https://aka.ms/az-edge-module-export-linux-amd64?$1" -O az-edge-module-export
-    wget "https://aka.ms/az-edge-site-scale-linux-amd64?$1" -O az-edge-site-scale
+    echo "Unsupported OS"
+    exit 1
 fi
-
