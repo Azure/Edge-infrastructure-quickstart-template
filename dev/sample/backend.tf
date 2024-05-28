95b68cbc9df6ddda0f9ffe85376565d21f605d5e
Agora Dev <dev@agora.com>
2024-05-28 15:20:26 -0400

    updated backendtemplate.tf

--- dev/sample/backend.tf
+++ dev/sample/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "AdaptiveCloud-IaC"
     storage_account_name = "adaptivecloudiac"
-    container_name       = "iac"
+    container_name       = "ac-iac"
     key                  = "sample.tfstate"
   }
 }
