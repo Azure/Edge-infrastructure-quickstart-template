969c8f4d350bdd2d5e579c3a89cd16f4ecf2d2d1
Zidong Lu <zilu@microsoft.com>
2024-05-29 14:23:02 +0800

    fix var in ip check

--- modules/base/checks.tf
+++ modules/base/checks.tf
@@ -1,5 +1,5 @@
 data "external" "lnetIpCheck" {
-  program = ["powershell.exe", "-File", "${abspath(path.module)}/scripts/ip-range-overlap.ps1", var.startingAddress, var.endingAddress, var.aksArc-lnet-startingAddress, var.aksArc-lnet-endingAddress]
+  program = ["powershell.exe", "-File", "${abspath(path.module)}/scripts/ip-range-overlap.ps1", var.startingAddress, var.endingAddress, var.lnet-startingAddress, var.lnet-endingAddress]
 
   lifecycle {
     postcondition {
