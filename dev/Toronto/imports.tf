a6b1d552cf6f54de7eb8a7970b39df64a8309668
Michael Godfrey <migodfre@microsoft.com>
2024-07-18 09:52:15 -0400

    changed Cal to Tor

--- dev/Toronto/imports.tf
+++ dev/Toronto/imports.tf
@@ -1,6 +1,6 @@
  # Uncomment the following lines to import the resource group when Arc servers are provisioned by yourself.
 
  import {
-   id = "/subscriptions/fbaf508b-cb61-4383-9cda-a42bfa0c7bc9/resourceGroups/California"
+   id = "/subscriptions/fbaf508b-cb61-4383-9cda-a42bfa0c7bc9/resourceGroups/Toronto"
    to = module.base.azurerm_resource_group.rg
  }
