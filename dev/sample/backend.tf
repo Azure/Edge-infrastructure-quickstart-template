e616563b69aca26a8a29a7eb0a6a884ca8afd005
Jason Tayler <jason.tayler@coles.com.au>
2024-07-30 13:02:19 +1000

    Added Setup information

--- dev/sample/backend.tf
+++ dev/sample/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "<ResourceGroupName>"
-    storage_account_name = "<StorageAccountName>"
-    container_name       = "<StorageContainerName>"
+    resource_group_name  = "RG_SUPERMARKETSTORES_HCIPLATFORM_SV1407_NONPROD_AUE"
+    storage_account_name = "storesiacterraform1407"
+    container_name       = "terraformiac"
     key                  = "sample.tfstate"
   }
 }
