323295ff45c4cec024bcacc35f9b855785c3dac2
Zidong Lu <zilu@microsoft.com>
2024-08-06 13:35:55 +0800

    remove PRIVATE_PREVIEW_SAS

--- private_preview.ps1
+++ private_preview.ps1
@@ -1,6 +1,2 @@
-param (
-    [string]$PRIVATE_PREVIEW_SAS
-)
-
-Invoke-WebRequest "https://aka.ms/az-edge-module-export-windows-amd64?$PRIVATE_PREVIEW_SAS" -OutFile az-edge-module-export.exe
-Invoke-WebRequest "https://aka.ms/az-edge-site-scale-windows-amd64?$PRIVATE_PREVIEW_SAS" -OutFile az-edge-site-scale.exe
+Invoke-WebRequest "https://aka.ms/az-edge-module-export-windows-amd64" -OutFile az-edge-module-export.exe
+Invoke-WebRequest "https://aka.ms/az-edge-site-scale-windows-amd64" -OutFile az-edge-site-scale.exe
