6396bd87cc4e109400309e8214fe4ca752f873b9
Zidong Lu <zilu@microsoft.com>
2024-08-06 13:37:18 +0800

    remove parameter in private preview sh

--- private_preview.sh
+++ private_preview.sh
@@ -2,16 +2,16 @@ arch=$(uname -m)
 
 if [[ $OSTYPE == *"linux"* ]]; then
     if [[ $arch == *"arm"* ]]; then
-        wget "https://aka.ms/az-edge-module-export-linux-arm64?$1" -O az-edge-module-export
-        wget "https://aka.ms/az-edge-site-scale-linux-arm64?$1" -O az-edge-site-scale
+        wget "https://aka.ms/az-edge-module-export-linux-arm64" -O az-edge-module-export
+        wget "https://aka.ms/az-edge-site-scale-linux-arm64" -O az-edge-site-scale
     else
-        wget "https://aka.ms/az-edge-module-export-linux-amd64?$1" -O az-edge-module-export
-        wget "https://aka.ms/az-edge-site-scale-linux-amd64?$1" -O az-edge-site-scale
+        wget "https://aka.ms/az-edge-module-export-linux-amd64" -O az-edge-module-export
+        wget "https://aka.ms/az-edge-site-scale-linux-amd64" -O az-edge-site-scale
     fi
 elif [[ $OSTYPE == *"darwin"* ]]; then
     if [[ $arch == *"arm"* ]]; then
-        wget "https://aka.ms/az-edge-module-export-darwin-arm64?$1" -O az-edge-module-export
-        wget "https://aka.ms/az-edge-site-scale-darwin-arm64?$1" -O az-edge-site-scale
+        wget "https://aka.ms/az-edge-module-export-darwin-arm64" -O az-edge-module-export
+        wget "https://aka.ms/az-edge-site-scale-darwin-arm64" -O az-edge-site-scale
     else
         echo "Unsupported architecture for macOS"
     fi
