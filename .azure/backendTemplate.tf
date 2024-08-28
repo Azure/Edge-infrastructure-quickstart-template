16df9ec3fe00237a9daa5e81ec88380dc1464d22
Zidong Lu <zilu@microsoft.com>
2024-08-28 13:01:57 +0800

    Add TODOs in backend template

--- .azure/backendTemplate.tf
+++ .azure/backendTemplate.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "<ResourceGroupName>"
-    storage_account_name = "<StorageAccountName>"
-    container_name       = "<StorageContainerName>"
+    resource_group_name  = "<ResourceGroupName>"    # TODO: Replace with your Resource Group Name
+    storage_account_name = "<StorageAccountName>"   # TODO: Replace with your Storage Account Name
+    container_name       = "<StorageContainerName>" # TODO: Replace with your Storage Container Name
     key                  = "{{.GroupName}}.tfstate"
   }
 }
+
+# TODO: run `git config --local core.hooksPath ./.azure/hooks/` to enable automatic backend configuration
