ebf56827f94ded66d401058709d9e5de1e08c324
Zidong Lu <zilu@microsoft.com>
2024-09-29 16:09:11 +0800

    update template (#128)

--- modules/base/variables.hci-vm-gallery-image.global.tf
+++ modules/base/variables.hci-vm-gallery-image.global.tf
@@ -1,7 +1,7 @@
 variable "download_win_server_image" {
   description = "Whether to download Windows Server image"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "user_storage_id" {
