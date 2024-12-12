f230b4ac0b335e1f4512450f1736e4e93f63adae
Junda Chen <jundachen@microsoft.com>
2024-12-12 15:36:08 +0800

    add storage account info

--- .azure/backendTemplate.tf
+++ .azure/backendTemplate.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "<ResourceGroupName>"
-    storage_account_name = "<StorageAccountName>"
-    container_name       = "<StorageContainerName>"
+    resource_group_name  = "jundachen"
+    storage_account_name = "jundacheniac"
+    container_name       = "iac"
     key                  = "{{.GroupName}}.tfstate"
   }
 }
